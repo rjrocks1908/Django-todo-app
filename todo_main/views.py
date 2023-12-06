3d62d2f32865f3dfb9c49702a20e7a538b42e275
Rahul Jain <rj.rahul1908@gmail.com>
2023-12-06 18:37:10 +0530

    added addTask

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,10 +3,7 @@ from todo.models import Task
 
 
 def home(request):
-    tasks = Task.objects.filter(is_completed=False)
+    tasks = Task.objects.filter(is_completed=False).order_by("-updated_at")
     completed_tasks = Task.objects.filter(is_completed=True)
-    contents = {
-        "tasks": tasks,
-        "completed_tasks": completed_tasks
-    }
+    contents = {"tasks": tasks, "completed_tasks": completed_tasks}
     return render(request, "home.html", contents)
