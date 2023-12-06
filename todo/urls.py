262c614d282ba53251d2e8d1cb3652cc2692fbfd
Rahul Jain <rj.rahul1908@gmail.com>
2023-12-06 19:31:57 +0530

    edit todo

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("addTask/", views.addTask, name="addTask"),
+    path("mark-as-done/<int:id>/", views.mark_as_done, name="mark_as_done"),
+    path("mark-as-undone/<int:id>/", views.mark_as_undone, name="mark_as_undone"),
+    path("edit_task/<int:id>", views.edit_task, name="edit_task"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404, render
 from todo.models import Task
 
 
-# Create your views here.
 def addTask(request):
     task = request.POST["task"]
     Task.objects.create(task=task)
     return redirect("home")
+
+
+def mark_as_done(request, id):
+    task = get_object_or_404(Task, id=id)
+    task.is_completed = True
+    task.save()
+    return redirect("home")
+
+
+def mark_as_undone(request, id):
+    task = get_object_or_404(Task, id=id)
+    task.is_completed = False
+    task.save()
+    return redirect("home")
+
+
+def edit_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    if request.method == "POST":
+        task.task = request.POST["task"]
+        task.save()
+        return redirect("home")
+    else:
+        context = {"task": task}
+        return render(request, "edit_task.html", context)
